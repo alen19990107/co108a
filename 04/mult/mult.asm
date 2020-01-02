4a4af1824cbd7a1d989ce2a11765b78a2372241d
alen19990107 <38763703+alen19990107@users.noreply.github.com>
2020-01-02 11:24:53 +0800

    Add files via upload

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,34 +7,39 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
-
- //      int R0=5; 
- //      int R1=4;
- //      int R2=0;
-@0
-D=A
-@R2
-M=D
+//int main() {
+//    int R0 = 3;
+//    int R1 = 5;
+//    int R2 = 0;
+//    
+//    while (R0 > 0) {    loop
+//      R2 = R2 + R1;
+//      R0 = R0 - 1;
+//      printf("R0=%d R1=%d R2=%d\n", R0, R1, R2);
+//    }
+//    
+//    printf("R2=%d\n", R2);
+//}
+    @2
+    M=0
     
-//loop:
-(loop)
- //      if (R0 <= 0) goto end;
-@R0
-D=M  // R0=D
-@end
-D;JLE  // D<=0 跳到end
- //      R2=R2+R1;
-@R1
-D=M  //R1=D
-@R2
-M=M+D  //R2=R2+D
- //      R0=R0-1;
-@R0
-M=M-1  //R0=R0-1
- //      goto loop;
-@loop
-0;JMP  //不管甚麼狀況都跳到loop
- //      end:
-(end)
-@end
-0;JMP  //在最後做一個無窮迴圈
+
+(LOOP)
+    @0
+    D=M
+    @END
+    D;JLE
+
+    @1
+    D=M
+    @2
+    M=D+M
+    @0
+    M=M-1
+    @LOOP
+    0;JMP
+    
+    @END
+(END)
+    @END
+    0;JMP
